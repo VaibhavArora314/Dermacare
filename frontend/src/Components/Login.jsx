72ca4547d0e83080a0d11f067aced66c9d183ebc
Srijan <srijan.03sri@gmail.com>
2023-09-23 00:30:33 +0530

    signup-form

--- frontend/src/Components/Login.jsx
+++ frontend/src/Components/Login.jsx
@@ -5,10 +5,14 @@ import { GoogleLogin } from "@react-oauth/google";
 import jwt_decode from "jwt-decode";
 import "./login.scss";
 import axios from "axios";
+import Signup from "./Signup";
+
 export default function Login() {
   const [rememberMe, setRememberMe] = useState(false);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [signup, setsignup] = useState(false);
+  const [error, seterror] = useState(false);
 
   const handleLogin = async () => {
     try {
@@ -28,74 +32,97 @@ export default function Login() {
       console.log("API Response:", response); // You can log the response for debugging
     } catch (error) {
       console.error("Login failed:", error);
+      seterror(true);
     }
   };
 
   console.log(email);
-  return (
-    <Grid container>
-      <div className="form-parent">
-        <div className="heading">
-          <p>Log in</p>
-        </div>
-        <div className="row row-1">
-          <div className="input-field">
-            <GoogleOAuthProvider clientId="521618851477-ge9n1u2p7sdp5m4aklj4i6lso0gob5ru.apps.googleusercontent.com">
-              <GoogleLogin
-                onSuccess={(credentialResponse) => {
-                  const details = jwt_decode(credentialResponse.credential);
-                  console.log(details);
-                  console.log(credentialResponse);
-                }}
-                onError={() => {
-                  console.log("Login Failed");
-                }}
-                className="Google-btn"
+  return !signup ? (
+    <>
+      <Grid container>
+        <div className="form-parent">
+          <div className="heading">
+            <p>
+              Log in{" "}
+              {error ? (
+                <span
+                  style={{
+                    fontWeight: 400,
+                    fontSize: "0.7em",
+                    color: "red",
+                  }}
+                >
+                  {" "}
+                  * user not registered kindly signup
+                </span>
+              ) : (
+                ""
+              )}
+            </p>
+          </div>
+          <div className="row row-1">
+            <div className="input-field">
+              <GoogleOAuthProvider clientId="521618851477-ge9n1u2p7sdp5m4aklj4i6lso0gob5ru.apps.googleusercontent.com">
+                <GoogleLogin
+                  onSuccess={(credentialResponse) => {
+                    const details = jwt_decode(credentialResponse.credential);
+                    console.log(details);
+                    console.log(credentialResponse);
+                  }}
+                  onError={() => {
+                    console.log("Login Failed");
+                  }}
+                  className="Google-btn"
+                />
+              </GoogleOAuthProvider>
+            </div>
+          </div>
+          <div className="row ">
+            <div className="input-field">
+              <label htmlFor="title">Email </label>
+              <input
+                placeholder="Enter your email address"
+                type="email"
+                onChange={(e) => setEmail(e.target.value)}
+                required
               />
-            </GoogleOAuthProvider>
+            </div>
           </div>
-        </div>
-        <div className="row ">
-          <div className="input-field">
-            <label htmlFor="title">Email </label>
-            <input
-              placeholder="Enter your email address"
-              type="email"
-              onChange={(e) => setEmail(e.target.value)}
-              required
-            />
+          <div className="row">
+            <div className="input-field">
+              <label htmlFor="password">Password </label>
+              <input
+                placeholder="Enter your password"
+                type="password"
+                required
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
           </div>
-        </div>
-        <div className="row">
-          <div className="input-field">
-            <label htmlFor="password">Password </label>
-            <input
-              placeholder="Enter your password"
-              type="password"
-              required
-              onChange={(e) => setPassword(e.target.value)}
-            />
+          <div className="row">
+            <label>
+              <input
+                type="checkbox"
+                checked={rememberMe}
+                onChange={() => setRememberMe(!rememberMe)}
+              />
+              Remember Me
+            </label>
           </div>
-        </div>
-        <div className="row">
-          <label>
-            <input
-              type="checkbox"
-              checked={rememberMe}
-              onChange={() => setRememberMe(!rememberMe)}
-            />
-            Remember Me
-          </label>
-        </div>
 
-        <button className="login-btn" onClick={handleLogin}>
-          Log In
-        </button>
-        <div className="signup">
-          <p>Don't have an account?</p>
-          <button className="signup-btn">Sign Up</button>
+          <button className="login-btn" onClick={handleLogin}>
+            Log In
+          </button>
+          <div className="signup">
+            <p>Don't have an account?</p>
+            <button className="signup-btn" onClick={() => setsignup(!signup)}>
+              Sign Up
+            </button>
+          </div>
         </div>
-      </div>
-    </Grid>
+      </Grid>
+    </>
+  ) : (
+    <Signup />
   );
 }
---
+++ frontend/src/Components/Signup.jsx
@@ -0,0 +1,186 @@
+import React, { useState } from "react";
+import Grid from "@mui/material/Grid";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import { GoogleLogin } from "@react-oauth/google";
+import jwt_decode from "jwt-decode";
+import "./login.scss";
+import axios from "axios";
+import DatePicker from "react-datepicker";
+
+import "react-datepicker/dist/react-datepicker.css";
+import Login from "./Login";
+
+export default function Signup() {
+  const [name, setname] = useState();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [gender, setgender] = useState();
+  const [signup, setsignup] = useState(false);
+  const [dob, setdob] = useState();
+  const [profilePhoto, setprofilePhoto] = useState(null);
+
+  const handleRegister = async () => {
+    try {
+      // Create an object with email and password
+      const data = {
+        name,
+        dob,
+        gender,
+        email,
+        password,
+        profilePhoto,
+      };
+
+      // Replace 'your_api_endpoint' with the actual API endpoint you want to send the data to
+      const response = await axios.post(
+        "http://localhost:5000/api/login",
+        data
+      );
+
+      // Handle the API response here, e.g., set user authentication, redirect, etc.
+      console.log("API Response:", response); // You can log the response for debugging
+      setsignup(false);
+    } catch (error) {
+      console.error("Login failed:", error);
+    }
+  };
+
+  const handleImageChange = (event) => {
+    const file = event.target.files[0];
+
+    if (file) {
+      const reader = new FileReader();
+
+      reader.onload = (e) => {
+        // Store the image data in the state
+        setprofilePhoto(e.target.result);
+      };
+
+      reader.readAsDataURL(file);
+    }
+  };
+
+  const currentYear = new Date().getFullYear();
+  const minYear = currentYear - 60;
+  const maxYear = currentYear;
+  console.log(email);
+
+  return !signup ? (
+    <>
+      <Grid container>
+        <div className="form-parent signup-parent">
+          <div className="heading">
+            <p>Sign Up</p>
+          </div>
+          <div className="row-1">
+            <div className="signup-field">
+              <GoogleOAuthProvider clientId="521618851477-ge9n1u2p7sdp5m4aklj4i6lso0gob5ru.apps.googleusercontent.com">
+                <GoogleLogin
+                  onSuccess={(credentialResponse) => {
+                    const details = jwt_decode(credentialResponse.credential);
+                    console.log(details);
+                    console.log(credentialResponse);
+                  }}
+                  onError={() => {
+                    console.log("Login Failed");
+                  }}
+                  className="Google-btn"
+                />
+              </GoogleOAuthProvider>
+            </div>
+          </div>
+          <div
+            className="signup-row"
+            style={{ borderBottom: "1.5px solid black", padding: "0.5rem" }}
+          >
+            <div className=" row signup-field">
+              <label htmlFor="name">Name </label>
+              <input
+                placeholder="Enter your email address"
+                type="email"
+                onChange={(e) => setname(e.target.value)}
+                required
+              />
+            </div>
+            <div className="row signup-field">
+              <label htmlFor="dob">Dob</label>
+              <DatePicker
+                id="dob"
+                dateFormat="yyyy-MM-dd"
+                selected={dob}
+                placeholderText="Select a date"
+                showYearDropdown
+                showMonthDropdown
+                scrollableYearDropdown
+                scrollableMonthYearDropdown
+                yearDropdownItemNumber={60} // Number of years displayed in the dropdown
+                minDate={new Date(minYear, 0, 1)} // January 1st of the minimum year
+                maxDate={new Date(maxYear, 11, 31)}
+                onChange={(dob) => {
+                  console.log("Selected Date:", dob);
+
+                  setdob(dob);
+                }}
+                required
+              />
+            </div>
+            <div className="row  signup-field">
+              <label htmlFor="gender">Gender </label>
+              <select
+                name="gender"
+                id="gender"
+                onChange={(e) => setgender(e.target.value)}
+              >
+                <option value="default">Select</option>
+                <option value="Male">Male</option>
+                <option value="Female">Female</option>
+                <option value="Dont say">Prefer Not Say</option>
+              </select>
+            </div>
+            <div className="row  signup-field">
+              <label htmlFor="email">Email</label>
+              <input
+                placeholder="Enter your Email"
+                onChange={(e) => setEmail(e.target.value)}
+                type="email"
+                required
+              />
+            </div>
+            <div className="row  signup-field">
+              <label htmlFor="password">Password </label>
+              <input
+                placeholder="Enter your email address"
+                type="password"
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+            </div>
+            <div className="row  signup-field">
+              <label htmlFor="photo">Profile Photo</label>
+              <input
+                type="file"
+                id="prof-photo"
+                accept=".jpg,.jpeg,.png"
+                onChange={handleImageChange}
+                style={{ display: "none" }}
+                required
+              />
+              <p
+                className="custom-upload-pic"
+                onClick={() => document.getElementById("prof-photo").click()}
+              >
+                Upload Image
+              </p>
+            </div>
+          </div>
+
+          <button className="login-btn" onClick={handleRegister}>
+            Register
+          </button>
+        </div>
+      </Grid>
+    </>
+  ) : (
+    <Login />
+  );
+}
