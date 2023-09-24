6ec3dbb3b1c9e92acab9b54aa903bd979551f7b0
Srijan <srijan.03sri@gmail.com>
2023-09-25 00:36:15 +0530

    upload-sec-fixed

--- frontend/src/pages/UploadImage.jsx
+++ frontend/src/pages/UploadImage.jsx
@@ -4,17 +4,63 @@ import { AuthContext } from "../context/AuthContext";
 import "../assets/css/UploadImage.scss";
 import uploadIcon from "../assets/icons/Upload.png";
 import imageVector from "../assets/icons/image-vector.png";
+import PropTypes from "prop-types";
+import LinearProgress from "@mui/material/LinearProgress";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+
+function LinearProgressWithLabel(props) {
+  return (
+    <Box sx={{ display: "flex", alignItems: "center" }}>
+      <Box sx={{ width: "100%", mr: 1 }}>
+        <LinearProgress variant="determinate" {...props} />
+      </Box>
+      <Box sx={{ minWidth: 35 }}>
+        <Typography variant="body2" color="text.secondary">{`${Math.round(
+          props.value
+        )}%`}</Typography>
+      </Box>
+    </Box>
+  );
+}
+
+LinearProgressWithLabel.propTypes = {
+  /**
+   * The value of the progress indicator for the determinate and buffer variants.
+   * Value between 0 and 100.
+   */
+  value: PropTypes.number.isRequired,
+};
 
 export default function UploadImage() {
+  const [progress, setProgress] = useState(Array(2).fill(10));
+
   const [selectedFile, setSelectedFile] = useState(null);
   const [message, setMessage] = useState("");
+  const [images, setImages] = useState([]);
+  const [uploading, setUploading] = useState(false);
+  const [currentIndex, setCurrentIndex] = useState(0);
   const { token } = useContext(AuthContext);
+  const [uploadingImageIndex, setUploadingImageIndex] = useState(-1);
+  const [uploadedFileName, setUploadedFileName] = useState([]);
 
   const handleFileChange = (e) => {
-    setSelectedFile(e.target.files[0]);
+    const file = e.target.files[0];
+
+    if (file && !uploading) {
+      setSelectedFile(file);
+      setImages((prevImages) => [...prevImages, file]);
+      setProgress((prevProgress) => [...prevProgress, 0]);
+      setCurrentIndex(currentIndex + 1);
+      setUploadingImageIndex(currentIndex);
+      setUploading(true);
+      setUploadedFileName((prevFileNames) => [...prevFileNames, file.name]);
+    }
   };
 
   const handleUpload = async () => {
+    // along with upload image api done below vaibhav and harsh add report genrating wali api in this function only
+
     try {
       const formData = new FormData();
       formData.append("image", selectedFile);
@@ -41,44 +87,37 @@ export default function UploadImage() {
     }
   };
 
-  const [images, setImages] = useState([]);
-  const [trackers, setTrackers] = useState([false, false, false]);
-  const [uploading, setUploading] = useState(false);
-  const [currentIndex, setCurrentIndex] = useState(0);
+  // image-progress-bar loader
 
-  const handleImageChange = (event) => {
-    const selectedFile = event.target.files && event.target.files[0];
-    if (currentIndex < 3) {
-      const newImages = [...images];
-      newImages[currentIndex] = selectedFile;
-      setImages(newImages);
-
-      const newTrackers = [...trackers];
-      newTrackers[currentIndex] = true;
-      setTrackers(newTrackers);
-
-      setCurrentIndex(currentIndex + 1);
-      setUploading(true);
-      console.log("1st run");
-    }
-  };
-  // // Simulated upload progress
   useEffect(() => {
-    if (uploading) {
+    if (uploadingImageIndex >= 0 && uploadingImageIndex < images.length) {
+      // Simulated upload progress
+      let uploadProgress = [...progress];
       const timer = setInterval(() => {
-        // Simulate upload completion
-        const completed = trackers.filter((tracker) => tracker).length;
-        if (completed === 3) {
-          // All images uploaded
+        if (uploadProgress[uploadingImageIndex] < 100) {
+          uploadProgress[uploadingImageIndex] += 10; // Update the progress for the specific image
+          setProgress([...uploadProgress]);
+        } else {
+          clearInterval(timer);
           setUploading(false);
+
+          if (currentIndex < images.length - 1) {
+            setCurrentIndex(currentIndex + 1);
+            setProgress((prevProgress) => [...prevProgress, 0]);
+            setUploading(true);
+          }
         }
-      }, 2000); // Simulated 2 seconds for upload
-
-      return () => clearInterval(timer);
+      }, 1000); // Simulated 1 second for each 10% progress
     }
-  }, [uploading, trackers]);
+  }, [
+    currentIndex,
+    images,
+    uploading,
+    uploadingImageIndex,
+    progress,
+    uploadedFileName,
+  ]);
 
-  console.log(images, trackers);
   return (
     <>
       <div className="par-container">
@@ -110,6 +149,9 @@ export default function UploadImage() {
                   <button
                     className="input-btn"
                     onClick={() => {
+                      if (currentIndex === 2)
+                        return alert("Only 2 image uploads allowed");
+
                       document.getElementById("profile-picture").click();
                     }}
                     // onChange={(e) => handleImageChange(e)}
@@ -127,17 +169,37 @@ export default function UploadImage() {
               </div>
             </div>
           </div>
-          {images.map((image, index) => (
+          {images.map((img, index) => (
             <div key={index} className="progress-tracker">
-              <p>Uploading Image {index + 1}</p>
-              {trackers[index] && (
-                <div className="progress-bar">
-                  <div className="progress" style={{ width: "100%" }}></div>
-                </div>
+              {progress[index] === 100 ? (
+                <>
+                  <p className="upload ">Uploaded</p>
+                  <div className="progress-bar">
+                    <b style={{ color: "gray" }}>File: </b>
+                    {uploadedFileName[index]}
+                    <LinearProgressWithLabel value={progress[index]} />
+                  </div>
+                </>
+              ) : (
+                <>
+                  <p className="upload">Uploading...</p>
+                  <div className="progress-bar">
+                    Your file:
+                    <LinearProgressWithLabel value={progress[index]} />
+                  </div>
+                </>
               )}
             </div>
           ))}
-          <button onClick={handleUpload}>Generate Report</button>
+
+          <button
+            className={`generate-btn ${
+              images.length !== 2 ? "gen-inactive" : ""
+            }`}
+            onClick={handleUpload}
+          >
+            Generate Report
+          </button>
         </div>
         <div className="par-container__img-section">
           <img src={imageVector} alt="" className="img-vector" />
