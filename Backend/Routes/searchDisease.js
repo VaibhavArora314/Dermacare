28bcfdd67c765fa9db11a4cfa12577fbd211c3cd
Harsh <goyalh4164@gmail.com>
2023-09-28 13:21:13 +0530

    minor changes in search api

--- Backend/Routes/searchDisease.js
+++ Backend/Routes/searchDisease.js
@@ -7,11 +7,8 @@ async function searchDisease (req, res){
   
       // List of prompts for disease information search
       const prompts = [
-        `Provide 5 key points about ${disease}.`,
-        `List 5 common symptoms of ${disease}.`,
-        `Describe the causes and risk factors of ${disease}.`,
-        `Explain the available treatment options for ${disease}.`,
-        `Share any recent research findings related to ${disease}.`,
+        `Provide 5 key points , symtons and risk factors about ${disease}.`,
+        `Explain the available treatment options for ${disease}.`
       ];
   
       // Array to store the responses
@@ -26,11 +23,8 @@ async function searchDisease (req, res){
   
       // Response object with the collected disease information
       const response = {
-        "Key Points": responses[0].split("\n").slice(0, 5), // Get the first 5 points
-        "Common Symptoms": responses[1].split("\n").slice(0, 5), // Get the first 5 symptoms
-        "Causes and Risk Factors": responses[2].split("\n").slice(0, 5), // Get the first 5 causes
-        "Treatment Options": responses[3].split("\n").slice(0, 5), // Get the first 5 treatment options
-        "Research Findings": responses[4].split("\n").slice(0, 5), // Get the first 5 research findings
+        "Key Points": responses[0],
+        "Common Symptoms": responses[1]
       };
   
       return res.status(200).json(response);
