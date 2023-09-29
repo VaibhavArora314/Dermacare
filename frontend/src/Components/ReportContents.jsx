a6b9ae306edad60859c9bd87fc3124f2b91cecc4
VaibhavArora314 <vaibhav.afw@gmail.com>
2023-09-29 11:01:48 +0530

    report hospitals

--- frontend/src/Components/ReportContents.jsx
+++ frontend/src/Components/ReportContents.jsx
@@ -2,6 +2,37 @@ import { Avatar, Box } from "@mui/material";
 import React from "react";
 import PdfViewer from "./PdfButtons";
 
+const hospitals = [
+  [
+    "Dwarka Dermatologist , Pediatric Dermatologist , ( Skin Specislist )",
+    "Upper ground floor , plot no- 96, Sector 13, Dwarka, Delhi, 110078",
+    "http://www.kvsc.in/",
+  ],
+
+  [
+    "Dr. J S. Randhawa, Skin Specialist, Dermatologist, Skin Clinic",
+    "Randhawa's Skin & VD Centre, Flat No. 689, LIG OM Appartment, Sector-14, Pocket B, Dwarka, Delhi, 110075",
+    "https://drrandhawadermatologist.webs.com/",
+  ],
+
+  [
+    "Dr. Sumit Sethi - Dermatologist || Skin Specialist || Laser Hair Removal in Dwarka",
+    "Metro Station, Venkateshwar Hospital, Sector 18A, Dwarka, near Sector 12, Delhi 110075",
+    "https://www.dermastation.com/",
+  ],
+  [
+    "Dermosphere Clinic- on Apollo 24|7",
+    "Flat number 154, Akshardham Apartment, Sector 19, Pocket 3, Phase 1, Dwarka, New Delhi, Delhi, 110075",
+    "https://www.dermosphere.com/",
+  ],
+
+  [
+    "Twacha Skin Clinic",
+    "84 , Sector 12A , Near IGL Petrol Pump , Road opposite Bal Bharti School, Dwarka, New Delhi, Delhi 110078",
+    "https://twachaclinic.com/",
+  ],
+];
+
 const ReportContents = ({
   diseaseName,
   imageUrl,
@@ -106,6 +137,32 @@ const ReportContents = ({
           professional before making any decisions or taking any additional
           steps based on these results.
         </p>
+      </Box>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          width: 0.75,
+          justifyContent: "space-between",
+          alignItems: "flex-start",
+          marginTop: 2,
+        }}
+      >
+        <h3>Hospitals</h3>
+        {hospitals.map((hospital, i) => (
+          <>
+            {" "}
+            <h4>
+              <b>
+                {i + 1}.{" "}
+                <a href={hospital[2]} target="_blank">
+                  {hospital[0]}
+                </a>
+              </b>
+            </h4>
+            <p style={{ margin: 0 }}>{hospital[1]}</p>
+          </>
+        ))}
       </Box>
     </>
   );
