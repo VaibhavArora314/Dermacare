db9d0d8d99347452ae3f0b58c8e2ccdced83bbb8
Srijan <srijan.03sri@gmail.com>
2023-10-04 22:07:22 +0530

    final-touch

--- frontend/src/Components/Footer.jsx
+++ frontend/src/Components/Footer.jsx
@@ -6,68 +6,72 @@ import ArrowRightAltIcon from "@mui/icons-material/ArrowRightAlt";
 
 function Footer() {
   return (
-    <div className="footer-parent">
-      <div className="logo-info">
-        {/* <div className="a1"> */}
-        <img
-          src={logo}
-          alt=""
-          style={{
-            transform: "scale(0.52)",
-            borderRadius: "1rem",
-            position: "relative",
-            height: "20vh",
-            width: "40vw",
-            marginRight: "-7rem",
-          }}
-        />
-        <img
-          src={authentic}
-          alt="verified"
-          style={{
-            display: "flex",
-            transform: "scale(0.6)",
-            position: "absolute",
-            marginLeft: "2rem",
-          }}
-        />
-        {/* </div> */}
-      </div>
-      <div className="dermacare-goal">
-        <p className="desc">
-          At Dermacare, we're dedicated to helping you achieve clear, radiant
-          and glowing skin so you can live your life with confidence without any
-          stress
-        </p>
-        <div className="a2">
-          <p className="copyright">© 2023 Dermacare. All rights reserved.</p>
-        </div>
-      </div>
-      <div className="email">
-        <div className="b1">
-          <p className="tagline">
-            Our best skin & hair tips straight to your inbox!
+    <footer>
+      <div className="footer-parent">
+        <div className="dermacare-goal">
+          <p className="desc">
+            At Dermacare, we're dedicated to helping you achieve clear, radiant
+            and glowing skin so you can live your life with confidence without
+            any stress .
           </p>
         </div>
-        <div
-          className="b2"
-          style={{ position: "relative", display: "inline-block" }}
-        >
-          <input className="input" type="email" placeholder="Your email" />
-          <ArrowRightAltIcon
-            fontSize="large"
-            style={{
-              opacity: "0.8",
-              position: "absolute",
-              right: "10px",
-              top: "20%",
-              color: "black",
-              // transform: "translateY(-50%)",
-            }}
-          />
+
+        <div className="logo-info">
+          <div className="a1">
+            <img
+              src={logo}
+              alt=""
+              style={{
+                transform: "scale(0.52)",
+                borderRadius: "1rem",
+                position: "relative",
+                height: "20vh",
+                width: "40vw",
+                // marginRight: "4rem",
+              }}
+            />
+            <img
+              src={authentic}
+              alt="verified"
+              style={{
+                display: "flex",
+                transform: "scale(0.6)",
+                position: "absolute",
+                marginRight: "25rem",
+              }}
+            />
+          </div>
+          <div className="a2">
+            <p className="copyright">© 2023 Dermacare. All rights reserved.</p>
+          </div>
+        </div>
+
+        <div className="email">
+          <div className="b1">
+            <p className="tagline">
+              Our best skin & hair tips straight to your inbox!
+            </p>
+          </div>
+          <div
+            className="b2"
+            style={{ position: "relative", display: "inline-block" }}
+          >
+            <input className="input" type="email" placeholder="Your email" />
+            <ArrowRightAltIcon
+              fontSize="large"
+              style={{
+                opacity: "0.8",
+                position: "absolute",
+                right: "10px",
+                top: "20%",
+                color: "black",
+                // transform: "translateY(-50%)",
+              }}
+            />
+          </div>
         </div>
       </div>
-    </div>
+    </footer>
   );
 }
 
