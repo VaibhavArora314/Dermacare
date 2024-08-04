f13bc01d5ed15148ee55412372bdf3cf18a88bb0
VaibhavArora314 <vaibhav.afw@gmail.com>
2024-08-04 18:10:46 +0530

    remove env credentials

--- Backend/Routes/teams.js
+++ Backend/Routes/teams.js
@@ -1,5 +1,7 @@
+const DEVS = ["Ultimateutkarsh11", "VaibhavArora314", "goyalh4164", "TruecoderSri", "karanchhillar", "bpnt"]
+
 async function  teams (req, res) {
-    const team = process.env.DEVS.split(" ");
+    const team = DEVS;
     return res.status(200).json({
       team,
     });
